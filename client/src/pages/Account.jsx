e264a9b6ed3b506ef8c292119227964dc3d36a36
unknown <trahy032@gmail.com>
2023-11-20 09:18:59 +1030

    created appointment components

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,9 +1,11 @@
 import Profile from "../components/Profile";
+import Appointments from "../components/AppointmentCard";
 
 export default function Home() {
   return (
     <>
       <Profile />
+      <Appointments />
     </>
   );
 }
